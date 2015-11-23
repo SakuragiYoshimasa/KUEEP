3fd8f85e63aac651aaeb6a8f0699a917bbbfc338
SakuragiYoshimasa <ysrhsp@outlook.com>
2015-11-23 11:11:12 +0900

    initial commit

---
+++ KUEEP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KUEEP
+//
+//  Created by SakuragiYoshimasa on 2015/11/23.
+//  Copyright © 2015年 SakuragiYoshimasa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
